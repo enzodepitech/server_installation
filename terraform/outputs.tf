6f664979f866180e14515863a3b57ba63e0a6107
Enzo Durel <enzo.durel@epitech.eu>
2026-07-21 15:09:35 +0200

    Add outputs for nginxPM

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,14 @@
-output "guacamole_container_ip" {
-  description = "The internal IP address of the Guacamole container"
+output "nginx_proxy_manager_ip" {
+  description = "IP address of Nginx Proxy Manager"
+  value       = proxmox_virtual_environment_container.nginxpm.initialization[0].ip_config[0].ipv4[0].address
+}
+
+output "guacamole_ip" {
+  description = "IP address of Guacamole"
   value       = proxmox_virtual_environment_container.guacamole.initialization[0].ip_config[0].ipv4[0].address
 }
 
 output "guacamole_web_url" {
-  description = "The internal web access URL for Guacamole"
+  description = "URL for Guacamole"
   value       = "http://${proxmox_virtual_environment_container.guacamole.initialization[0].ip_config[0].ipv4[0].address}:8080/guacamole"
 }
