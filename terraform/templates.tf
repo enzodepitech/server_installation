54308759e40aacf3ff4e22f9025f2d14af4be466
Enzo Durel <enzo.durel@gmail.com>
2026-07-24 10:10:08 +0200

    Fix dashboard guacamole url & overwrite debian template terraform

--- terraform/templates.tf
+++ terraform/templates.tf
@@ -3,4 +3,5 @@ resource "proxmox_download_file" "debian_13_template" {
   datastore_id = "local"
   node_name    = "pve"
   url          = "http://download.proxmox.com/images/system/debian-13-standard_13.6-1_amd64.tar.zst"
+  overwrite    = true
 }
